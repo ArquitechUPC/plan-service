15711e98c81b3001fc248d7c6d6da5081c059850
RamiroVT <u202123714@upc.edu.pe>
2024-04-28 15:43:19 -0500

    add: swagger implementation

--- src/main/java/org/Arquitech/Gymrat/planservice/PlanServiceApplication.java
+++ src/main/java/org/Arquitech/Gymrat/planservice/PlanServiceApplication.java
@@ -1,9 +1,19 @@
 package org.Arquitech.Gymrat.planservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Plan - GymRat",
+				version = "1.0",
+				description = "API to manage plans part of the application called GymRat"
+		)
+)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PlanServiceApplication {
